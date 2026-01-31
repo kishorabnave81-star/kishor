4e410068c90e75023ab4501126368a94bd19d3e7
Kishor <kishor.abnave81@gmail.com>
2026-01-31 14:53:08 +0530

    Added src folder and react setup

---
+++ App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">
+        My Portfolio is Live 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
